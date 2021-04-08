ef3b32e139dd0084b566339408705a43d172f277
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2021-04-08 22:55:48 +0300

    Доделал слайдеры Recommended и For you. Настроил слайдеры, чтобы переключались отдельно. В слайдер For you добавил кнопку Sunscribe со счётчиком подписчиков.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,43 @@ const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 5,
   spaceBetween: 40,
 
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
+  },
+
+});
+
+const recommendedSwiper = new Swiper('.recommended-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+
+});
+
+const forYouSwiper = new Swiper('.foryou-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 40,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.foryou-button-next',
+    prevEl: '.foryou-button-prev',
   },
 
 });
